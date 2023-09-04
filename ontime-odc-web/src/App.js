e1791a3873154f6124f2c3d168719d6eef7520e2
James1997-Ed <johnnyk.sns@gmail.com>
2023-09-04 14:02:02 +0000

    new Change added

---
+++ ontime-odc-web/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='text-center text-2xl font-bold'>Welcome </h1>
+    </div>
+  );
+}
+
+export default App;
