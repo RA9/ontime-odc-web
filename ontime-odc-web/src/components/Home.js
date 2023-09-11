077b8b9f82f2c0c4958fc3a76f1be4dca56f5463
Josephineb-Holder <89983188+Josephineb-Holder@users.noreply.github.com>
2023-09-11 12:46:14 +0000

    features description

--- ontime-odc-web/src/components/Home.js
+++ ontime-odc-web/src/components/Home.js
@@ -5,28 +5,29 @@ import NavBar from "./NavBar";
 
 const Home = () => {
   const ImgData = [
-    {
-      text: ` OnTime visitor management app is to provide a comprehansive
-      solution for managing visitors entering and exiting the Orange
-      Digital Center(ODC). The app aims to enhance the visitor
-      registration process, improve security, and offer insight
-      through the visitor tricking.`,
+    { head:`Events And Courses`,
+      text: ` OnTime Visitor's Management application provides 
+      relevent information about upcoming courses and events at
+       the orange digital center Liberia.The Orange digital center 
+       offers a wide range of technology courses and events at no
+        cost periodically, to support the tech enthusissts in liberia.`,
     },
     {
-      text: ` OnTime visitor management app is to provide a comprehansive
-      solution for managing visitors entering and exiting the Orange
-      Digital Center(ODC). The app aims to enhance the visitor
-      registration process, improve security, and offer insight
-      through the visitor tricking.`,
+      head:`Attendence Tracking`,
+      text: ` The OnTime app tracks realtime visitors employees and students attendence.
+      It provides a structured and reliable system to collect and store data.
+      This brings employees attendence data to management's fingers and helps in making quality decisions and for reference purposes.`,
     },
     {
-      text: ` OnTime visitor management app is to provide a comprehansive
-      solution for managing visitors entering and exiting the Orange
-      Digital Center(ODC). The app aims to enhance the visitor
-      registration process, improve security, and offer insight
-      through the visitor tricking.`,
+      head:`Activities Tracking`,
+      text: ` OnTime management app collects and track the activities
+       of every visitor, student and facilitator that visits the orange digital
+        center daily. This provides management with statistical data about the how 
+        students and participants are turning up for courses and events at the ODC.
+         `,
     },
     {
+      head:`Events And Courses`,
       text: ` OnTime visitor management app is to provide a comprehansive
       solution for managing visitors entering and exiting the Orange
       Digital Center(ODC). The app aims to enhance the visitor
@@ -35,10 +36,11 @@ const Home = () => {
     },
   ];
 
-  const TextProp = ({ text }) => {
+  const TextProp = ({ text , head}) => {
     return (
       <div className="justify-center p-10 ">
         {/* <img src={image} alt="img" width="200" height="100" /> */}
+        <h1 className="text-2xl font-bold">{head}</h1>
         <p>{text}</p>
       </div>
     );
@@ -78,7 +80,7 @@ const Home = () => {
               key={index}
               className="sm:m-auto shadow-2xl w-full lg:w-1/5 mb-10 rounded-xl text-gray-600 bg-gray-50"
             >
-              <TextProp text={item.text} />
+              <TextProp head={item.head} text={item.text} />
             </div>
           ))}
         </div>
