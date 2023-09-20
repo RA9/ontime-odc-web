b559d56dc89115adac3197632830137ab74b1237
James1997-Ed <johnnyk.sns@gmail.com>
2023-09-20 12:37:28 +0000

    added new changes to the branch

--- ontime-odc-web/src/components/About.js
+++ ontime-odc-web/src/components/About.js
@@ -9,41 +9,53 @@ const About = () => {
 
     const Data = [
         {
-            name: 'Josephine P. Holder',
+            name: 'Nyenpandi J. B. Holder',
             image: '/images/Josephine.png',
-            text: "I am a developer.",
+            text: "Student",
+            link: "",
         },
         {
             name: 'Johnny Kpehe',
             image: '/images/migee.png',
-            text: "I am a developer.",
+            text: "Student.",
+            link: "https://github.com/James1997-Ed?tab=repositories",
+            link2: "https://twitter.com/johnny_k1997",
+            link3: "https://www.linkedin.com/in/johnny-kpehe-90607a254/",
         },
         {
             name: 'Princess Washington',
             image: '/images/princess.png',
-            text: "I am a developer.",
+            text: "Student",
+            link: "https://github.com/princesswG",
         },
         {
-          name: 'Logic',
+          name: 'Alfred Ngwayah',
           image: '/images/logic.png',
-          text: "I am a developer.",
+          text: "Student",
+          link: "https://github.com/Big-Logic"
       },
         {
-            name: 'Dev Iskalo',
+            name: 'Isaac L. Kamara, Jr',
             image: '/images/David.png',
-            text: "I am a developer.",
+            text: "Student",
+            link: "https://github.com/Deviskalo"
         }
     ]
 
 
-    function UserProfile({name,image,text}){
+    function UserProfile({name,image,text, link, link2, link3}){
 
         return(
             <div className=' justify-items-center  block sm:p-2 m-2 rounded-xl'>
-               <div className='shadow-2xl rounded-2xl p-4  text-center'>
-               <img className='m-auto rounded-full outline outline-offset-2 outline-white' src={image} alt="user img" width="200" height="200"/>
+               <div className='shadow-2xl rounded-2xl p-4  text-center bg-slate-800'>
+               <img className='m-auto rounded-full  outline-white' src={image} alt="user img" width="200" height="200"/>
                  <h1 className='text-xl text-center font-bold p-2 text-orange-600'>{name}</h1>
-                 <p className=' text-center text-white'>{text}</p>
+                 <p className=' text-center text-white mb-2'>{text}</p>
+                 <div className='flex justify-center'>
+                 <a href={link}><img src='/images/github.png' alt='images' width='30' heigh='50' className='m-auto mr-2 rounded-2xl'/></a>
+                 <a href={link2}><img src='/images/twitter.png' alt='images' width='30' heigh='50' className='m-auto mr-2 rounded-2xl'/></a>
+                 <a href={link3}><img src='/images/linkin.png' alt='images' width='30' heigh='50' className='m-auto mr-2 rounded-2xl' /></a>
+                 </div>
                </div>
             </div>
         )
@@ -56,28 +68,31 @@ const About = () => {
       </header>
       <div className='sm:flex block justify-center shadow-2xl sm:p-16 rounded-2xl  sm:m-12 m-2 bg-slate-200 '>
       <div className=' sm:w-[1500px] bg-gray-300 sm:p-8 p-4 sm:rounded-2xl'>
-        <img  src='/images/Ontime2.png' width='700'  alt='img' className='m-auto w-[200px] rounded-lg shadow-2xl'/>
+        <img  src='/images/Ontime2.png' width='800'  alt='img' className='m-auto w-[200px] rounded-lg shadow-2xl'/>
          </div>
          <div className='sm:p-8 p-2 m-4 '>
         <p className='sm:text-2xl text-xl'>OnTime visitor management app is to provide a comprehansive solution for managing visitors entering and exiting the Orange Digital Center(ODC).
         The app aims to enhance the visitor registration process, improve security, and offer insight through the visitor tricking. </p>
 
-        <div className='sm:block flex'>
-           <h1 className='sm:mt-12 mt-4 sm:text-2xl font-bold '>Download</h1><AiOutlineArrowDown className='text-xl animate-bounce w-6 h-6'/>
-          <Link to='/download'><button className='bg-orange-600 rounded-2xl text-white shadow-xl sm:text-xl text-center p-2 sm:m-8 m-2'>Apple Store</button></Link>
-          <Link to='/download'><button className='bg-orange-600 rounded-2xl text-white shadow-xl sm:text-xl text-center p-2 sm:m-8 m-2'>Play Store</button></Link>
+        <div className='sm:grid grid-cols-2 sm:m-8'>
+          <Link to='/download'><img className='m-2' src="/images/appstore.png" alt="newImage"/></Link>
+          <Link to='/download'><img className='rounded-xl m-2' src="/images/playstore.png" alt="newImage"/></Link>
         </div>
     </div>
   </div>
   <h1 className='text-center font-bold sm:text-5xl text-4xl'>Meet Our Team</h1>
-     <div className='grid sm:grid-cols-3 smp-8 sm:m-8 rounded-2xl m-2 bg-slate-950'>
+     <div className='grid sm:grid-cols-3 sm:p-8 sm:m-8 rounded-2xl m-2 bg-slate-950'>
           {
             Data.map((item, index)=> (
                 <div key={index}>
                   <UserProfile
                     name={item.name} 
                     text={item.text} 
-                    image={item.image}/>
+                    image={item.image}
+                    link={item.link}
+                    link2={item.link2}
+                    link3={item.link3}
+                    />
                 </div>
             ))
           }
